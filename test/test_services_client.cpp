a644255c4880cbbba55197a890c85b60179a67c0
Tanmay <tanmay.n.haldankar@gmail.com>
2022-11-30 20:14:07 -0500

    Updating cpplint files

--- test/test_services_client.cpp
+++ test/test_services_client.cpp
@@ -1,55 +1,54 @@
-// Copyright 2015 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @file test_services_client.cpp
+ * @author Tanmay Haldankar (tanmay.n.haldankar99@gmail.com)
+ * @brief  modified test client file to test service
+ * @version 0.1
+ * @date 2022-11-30
+ *
+ * @copyright MIT License (c)
+ *
+ */
 
 #include <chrono>
 #include <memory>
 
+#include "beginner_tutorials/srv/print_new_string.hpp"
 #include "gtest/gtest.h"
-
 #include "rclcpp/exceptions.hpp"
 #include "rclcpp/rclcpp.hpp"
 
-#include "beginner_tutorials/srv/print_new_string.hpp"
-
 #ifdef RMW_IMPLEMENTATION
-# define CLASSNAME_(NAME, SUFFIX) NAME ## __ ## SUFFIX
-# define CLASSNAME(NAME, SUFFIX) CLASSNAME_(NAME, SUFFIX)
+#define CLASSNAME_(NAME, SUFFIX) NAME##__##SUFFIX
+#define CLASSNAME(NAME, SUFFIX) CLASSNAME_(NAME, SUFFIX)
 #else
-# define CLASSNAME(NAME, SUFFIX) NAME
+#define CLASSNAME(NAME, SUFFIX) NAME
 #endif
 
 using namespace std::chrono_literals;
 
-class CLASSNAME (test_services_client, RMW_IMPLEMENTATION) : public ::testing::Test
-{
-public:
-  static void SetUpTestCase()
-  {
-    rclcpp::init(0, nullptr);
-  }
+/**
+ * @brief Class to setup test cases for service
+ *
+ */
+class CLASSNAME(test_services_client, RMW_IMPLEMENTATION)
+    : public ::testing::Test {
+ public:
+  static void SetUpTestCase() { rclcpp::init(0, nullptr); }
 
-  static void TearDownTestCase()
-  {
-    rclcpp::shutdown();
-  }
+  static void TearDownTestCase() { rclcpp::shutdown(); }
 };
 
+/**
+ * @brief Create test to test service and client
+ *
+ */
 TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_noreqid) {
   auto node = rclcpp::Node::make_shared("test_services_client_no_reqid");
 
-  auto client = node->create_client<beginner_tutorials::srv::PrintNewString>("add_two_ints_noreqid");
-  auto request = std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
+  auto client = node->create_client<beginner_tutorials::srv::PrintNewString>(
+      "add_two_ints_noreqid");
+  auto request =
+      std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
   request->s = "Message";
 
   if (!client->wait_for_service(20s)) {
@@ -58,17 +57,24 @@ TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_noreqid) {
 
   auto result = client->async_send_request(request);
 
-  auto ret = rclcpp::spin_until_future_complete(node, result, 5s);  // Wait for the result.
+  auto ret = rclcpp::spin_until_future_complete(node, result,
+                                                5s);  // Wait for the result.
   ASSERT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
 
   EXPECT_EQ("Message", result.get()->new_string);
 }
 
+/**
+ * @brief Create test to test service and client
+ *
+ */
 TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_reqid) {
   auto node = rclcpp::Node::make_shared("test_services_client_add_reqid");
 
-  auto client = node->create_client<beginner_tutorials::srv::PrintNewString>("add_two_ints_reqid");
-  auto request = std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
+  auto client = node->create_client<beginner_tutorials::srv::PrintNewString>(
+      "add_two_ints_reqid");
+  auto request =
+      std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
   request->s = "Message";
 
   if (!client->wait_for_service(20s)) {
@@ -77,18 +83,25 @@ TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_reqid) {
 
   auto result = client->async_send_request(request);
 
-  auto ret = rclcpp::spin_until_future_complete(node, result, 5s);  // Wait for the result.
+  auto ret = rclcpp::spin_until_future_complete(node, result,
+                                                5s);  // Wait for the result.
   ASSERT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
 
   EXPECT_EQ("Message", result.get()->new_string);
 }
 
-TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_return_request) {
+/**
+ * @brief Create test to test return request
+ *
+ */
+TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION),
+       test_return_request) {
   auto node = rclcpp::Node::make_shared("test_services_client_return_request");
 
   auto client = node->create_client<beginner_tutorials::srv::PrintNewString>(
-    "add_two_ints_reqid_return_request");
-  auto request = std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
+      "add_two_ints_reqid_return_request");
+  auto request =
+      std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
   request->s = "Message";
 
   if (!client->wait_for_service(20s)) {
@@ -96,22 +109,31 @@ TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_return_request)
   }
 
   auto result = client->async_send_request(
-    request,
-    [](rclcpp::Client<beginner_tutorials::srv::PrintNewString>::SharedFutureWithRequest future) {
-      EXPECT_EQ("Message", future.get().first->s);
-      EXPECT_EQ("Message", future.get().second->new_string);
-    });
+      request, [](rclcpp::Client<beginner_tutorials::srv::PrintNewString>::
+                      SharedFutureWithRequest future) {
+        EXPECT_EQ("Message", future.get().first->s);
+        EXPECT_EQ("Message", future.get().second->new_string);
+      });
 
-  auto ret = rclcpp::spin_until_future_complete(node, result, 5s);  // Wait for the result.
+  auto ret = rclcpp::spin_until_future_complete(node, result,
+                                                5s);  // Wait for the result.
   ASSERT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
 }
 
-TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_two_ints_defered_cb) {
-  auto node = rclcpp::Node::make_shared("test_services_client_add_two_ints_defered_cb");
+/**
+ * @brief Create test to test the message being recieved across service and
+ * client
+ *
+ */
+TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION),
+       test_add_two_ints_defered_cb) {
+  auto node =
+      rclcpp::Node::make_shared("test_services_client_add_two_ints_defered_cb");
 
   auto client = node->create_client<beginner_tutorials::srv::PrintNewString>(
-    "add_two_ints_defered_cb");
-  auto request = std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
+      "add_two_ints_defered_cb");
+  auto request =
+      std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
   request->s = "Message";
 
   if (!client->wait_for_service(20s)) {
@@ -119,22 +141,31 @@ TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_two_ints_de
   }
 
   auto result = client->async_send_request(
-    request,
-    [](rclcpp::Client<beginner_tutorials::srv::PrintNewString>::SharedFutureWithRequest future) {
-      EXPECT_EQ("Message", future.get().first->s);
-      EXPECT_EQ("Message", future.get().second->new_string);
-    });
+      request, [](rclcpp::Client<beginner_tutorials::srv::PrintNewString>::
+                      SharedFutureWithRequest future) {
+        EXPECT_EQ("Message", future.get().first->s);
+        EXPECT_EQ("Message", future.get().second->new_string);
+      });
 
-  auto ret = rclcpp::spin_until_future_complete(node, result, 5s);  // Wait for the result.
+  auto ret = rclcpp::spin_until_future_complete(node, result,
+                                                5s);  // Wait for the result.
   ASSERT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
 }
 
-TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_two_ints_defcb_with_handle) {
-  auto node = rclcpp::Node::make_shared("test_services_client_add_two_ints_defered_cb_with_handle");
+/**
+ * @brief Create test to test the message being recieved across service and
+ * client
+ *
+ */
+TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION),
+       test_add_two_ints_defcb_with_handle) {
+  auto node = rclcpp::Node::make_shared(
+      "test_services_client_add_two_ints_defered_cb_with_handle");
 
   auto client = node->create_client<beginner_tutorials::srv::PrintNewString>(
-    "add_two_ints_defered_cb_with_handle");
-  auto request = std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
+      "add_two_ints_defered_cb_with_handle");
+  auto request =
+      std::make_shared<beginner_tutorials::srv::PrintNewString::Request>();
   request->s = "Message";
 
   if (!client->wait_for_service(20s)) {
@@ -142,12 +173,13 @@ TEST_F(CLASSNAME(test_services_client, RMW_IMPLEMENTATION), test_add_two_ints_de
   }
 
   auto result = client->async_send_request(
-    request,
-    [](rclcpp::Client<beginner_tutorials::srv::PrintNewString>::SharedFutureWithRequest future) {
-      EXPECT_EQ("Message", future.get().first->s);
-      EXPECT_EQ("Message", future.get().second->new_string);
-    });
+      request, [](rclcpp::Client<beginner_tutorials::srv::PrintNewString>::
+                      SharedFutureWithRequest future) {
+        EXPECT_EQ("Message", future.get().first->s);
+        EXPECT_EQ("Message", future.get().second->new_string);
+      });
 
-  auto ret = rclcpp::spin_until_future_complete(node, result, 5s);  // Wait for the result.
+  auto ret = rclcpp::spin_until_future_complete(node, result,
+                                                5s);  // Wait for the result.
   ASSERT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
 }
